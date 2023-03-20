c48b1a13fb26ab467f27eb725e2ae5cbc5e0d581
unknown <gbengalilmahn@gmail.com>
2023-03-20 22:47:08 +0100

    vercel.json

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/", require("./routes/userRoutes"));
 const connecteDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI); 
-    // console.log("connected to db");
+    console.log("connected to db");
   } catch (error) {
     console.log(error);
     process.exit(1);
